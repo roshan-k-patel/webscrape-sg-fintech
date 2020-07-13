a0ec3f1c70057a5ce14dc109777f3ff3390a8ba0
Roshan Patel <roshan.patel771@gmail.com>
2020-07-13 13:30:42 +0800

    fixed parsing same page bug

--- webscraping_sg_fintech.py
+++ webscraping_sg_fintech.py
@@ -6,10 +6,15 @@ from urllib.request import urlopen, Request
 links = []
 
 # iterates through each page and gets the directory link for each company
-def get_container_links(containers,browser,no_pages):
+def get_container_links(browser,no_pages):
     page = 1
 
     while page <= no_pages:
+
+        html = browser.page_source
+        page_soup = soup(html, 'html.parser')
+
+        containers = page_soup.findAll("div", {"class": "sabai-col-xs-9 sabai-directory-main"})
 
         print("Getting links for page: " + str(page))
 
@@ -17,7 +22,6 @@ def get_container_links(containers,browser,no_pages):
             link = container.find('a')['href']
             links.append(link)
 
-        # presses next page button
         try:
             next_page_buttons = browser.find_elements_by_class_name("sabai-btn.sabai-btn-default.sabai-btn-sm")
             next = next_page_buttons[len(next_page_buttons)-1]
@@ -28,7 +32,9 @@ def get_container_links(containers,browser,no_pages):
         except:
             pass
 
-        time.sleep(5)
+        containers.clear()
+
+        time.sleep(4)
 
         page = page + 1
 
@@ -45,26 +51,71 @@ def get_container_details(link):
         fin_cert_block = page_soup.find("div", {"class": "sabai-directory-field sabai-field-type-boolean sabai-field-name-field-fintech-certified sabai-clearfix"})
         uen_block = page_soup.find("div", {"class": "sabai-directory-field sabai-field-type-string sabai-field-name-field-uen sabai-clearfix"})
         tags_block = page_soup.find("div", {"class": "sabai-directory-category"})
-        tags_list = tags_block.findAll("a")
+        try:
+            tags_list = tags_block.findAll("a")
+        except:
+            pass
+
+        try:
+            biz_model_block = page_soup.find("div", {"class": "sabai-directory-field sabai-field-type-choice sabai-field-name-field-business-model sabai-clearfix"})
+            biz_model = biz_model_block.find("div", {"class": "sabai-field-value"}).text
+            biz_model = biz_model.replace(",",";").strip()
+        except:
+            biz_model = ''
+
+        try:
+            staff_count_block = page_soup.find("div", {"class": "sabai-directory-field sabai-field-type-choice sabai-field-name-field-staff-count sabai-clearfix"})
+            staff_count = staff_count_block.find("div", {"class": "sabai-field-value"}).text
+            staff_count = staff_count.replace("-"," to ").strip()
+        except:
+            staff_count = ''
+
+        try:
+            incorp_date_block = page_soup.find("div", {"class": "sabai-directory-field sabai-field-type-date-timestamp sabai-field-name-field-incorporation-date sabai-clearfix"})
+            incorp_date = incorp_date_block.find("div", {"class": "sabai-field-value"}).text
+            incorp_date = incorp_date.replace(",",";").strip()
+        except:
+            incorp_date = ''
+
+        try:
+            street_add_block = page_soup.find("div", {"class": "sabai-directory-field sabai-field-type-string sabai-field-name-field-address sabai-clearfix"})
+            street_add = street_add_block.find("div", {"class": "sabai-field-value"}).text
+            street_add = street_add.replace(",",";").strip()
+        except:
+            street_add = ''
+
+
         addresses_block = page_soup.find("div", {"class": "sabai-directory-contact"})
         addresses_list = addresses_block.findAll("a")
         description = '' + page_soup.find("p").text
-        description = description.replace(',','')
+        description = description.replace(',',';').replace("\n",'').strip()
+
 
         # detail extraction
         title = details_block.select("h1.fl-post-title")[0].text
-        fintech_certified = fin_cert_block.select("div.sabai-field-value")[0].text
-        uen = uen_block.select("div.sabai-field-value")[0].text
+        title = title.replace(',','').strip()
+        try:
+            fintech_certified = fin_cert_block.select("div.sabai-field-value")[0].text
+        except:
+            fintech_certified = ''
+        try:
+            uen = uen_block.select("div.sabai-field-value")[0].text
+        except:
+            uen = ''
+
         tags = ''
         emails = ''
         websites = ''
         description = description.lower().capitalize()
 
         # data manipulation for csv
-        for tag in tags_list:
-            tags = tags + ' -'+ tag.text
+        try:
+            for tag in tags_list:
+                tags = tags + ' -'+ tag.text
 
-        tags = tags[3:]
+            tags = tags[3:]
+        except:
+            tags = ''
 
         for address in addresses_list:
             if address.text.__contains__('@'):
@@ -75,14 +126,15 @@ def get_container_details(link):
         emails = emails[2:]
         websites = websites[2:]
 
-        return (title,tags,uen,fintech_certified,emails,websites,description)
+
+        return (title,tags,uen,fintech_certified,biz_model,staff_count,incorp_date,emails,websites,street_add,description)
 
 
 def main():
     # creates a file and writes the headings
     filename = "sg_fintech.csv"
     f = open(filename, "w")
-    headers = "Company Name, Tags, UEN, Fintech Certified, Email, Website, Description\n"
+    headers = "Company Name, Tags, UEN, Fintech Certified, Biz. Model, No. Staff, Inc. Date, Email, Website, Address, Description\n"
 
     f.write(headers)
 
@@ -92,25 +144,23 @@ def main():
     browser.get(address)
     time.sleep(5)
 
-    html = browser.page_source
-    page_soup = soup(html, 'html.parser')
-    containers = page_soup.findAll("div", {"class": "sabai-col-xs-9 sabai-directory-main"})
 
-    # gets all container links from each page
-    get_container_links(containers,browser,59)
-
+    get_container_links(browser,59)
     browser.quit()
-
-    get_container_details(links[0])
 
     # gets all details and writes them to the csv
     for iteration, link in enumerate(links):
         deets = get_container_details(link)
-        csv_string = deets[0] + "," + deets[1] + "," + deets[2] + "," + deets[3] + "," + deets[4] + "," + deets[5] + "," + deets[6]
-        f.write(csv_string + "\n")
 
-        print(str(iteration+1) + " / " + str(len(links)))
-        print(csv_string)
+
+        if "aggregated by medici" in str(deets):
+            pass
+        else:
+            csv_string = deets[0] + "," + deets[1] + "," + deets[2] + "," + deets[3] + "," + deets[4] + "," + deets[5] +\
+                         "," + deets[6] + "," + deets[7] + "," + deets[8] + "," + deets[9] + "," + deets[10]
+            f.write(csv_string + "\n")
+            print(str(iteration + 1) + " / " + str(len(links)))
+            print(csv_string)
 
     f.close()
 
